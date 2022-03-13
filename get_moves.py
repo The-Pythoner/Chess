dea707ddc94e062b1860264d9d86f8ffdc81d434
The Pythoner <82798026+The-Pythoner@users.noreply.github.com>
2022-03-13 13:04:28 +0100

    Add files via upload

--- get_moves.py
+++ get_moves.py
@@ -11,7 +11,7 @@ def is_in(i, j):
 	return i < 8 and i >= 0 and j < 8 and j >= 0
 
 @jit(nopython=True)
-def get_moves(array, turn, history):
+def get_moves_(array, turn, history):
 	moves = np.zeros((0, 6), dtype=np.int8)
 
 	for i in range(8):
@@ -102,26 +102,27 @@ def get_moves(array, turn, history):
 				if abs(array[k, 7]) != 2 or not is_turn(array[k, 7], turn):
 					right_cast = False
 
-				for h in range(history.shape[0]):
-					if abs(history[h, k, 4]) != 6 or not is_turn(history[h, k, 4], turn):
-						right_cast = False
-						left_cast = False
+				if history.shape[0] != 0:
+					for h in range(history.shape[0]):
+						if abs(history[h, k, 4]) != 6 or not is_turn(history[h, k, 4], turn):
+							right_cast = False
+							left_cast = False
 
-					if abs(history[h, k, 0]) != 2 or not is_turn(history[h, k, 0], turn):
-						left_cast = False
+						if abs(history[h, k, 0]) != 2 or not is_turn(history[h, k, 0], turn):
+							left_cast = False
 
-					if abs(history[h, k, 7]) != 2 or not is_turn(history[h, k, 7], turn):
-						right_cast = False
+						if abs(history[h, k, 7]) != 2 or not is_turn(history[h, k, 7], turn):
+							right_cast = False
 
 				if right_cast:
-					if array[i+1, j] == 0 and array[i+2, j] == 0:
+					if array[k, 5] == 0 and array[k, 6] == 0:
 						moves = np.vstack((moves, np.array(
-						[2, i, j, j+2, j+3, j+1], dtype=np.int8).reshape((1, 6))))
+						[2, k, 4, 6, 7, 5], dtype=np.int8).reshape((1, 6))))
 
 				if left_cast:
-					if array[i-1, j] == 0 and array[i-2, j] == 0 and array[i-3, j] == 0:
+					if array[k, 3] == 0 and array[k, 2] == 0 and array[k, 1] == 0:
 						moves = np.vstack((moves, np.array(
-						[2, i, j, j-2, j-4, j-1], dtype=np.int8).reshape((1, 6))))
+						[2, k, 4, 2, 0, 3], dtype=np.int8).reshape((1, 6))))
 
 			# rook or queen
 			if abs(p) in [2, 5] and is_turn(p, turn):
@@ -176,3 +177,51 @@ def get_moves(array, turn, history):
 								break
 
 	return moves
+
+@jit(nopython=True)
+def play(array, move):
+	if move[0] != 2:
+		array[move[3], move[4]] = array[move[1], move[2]]
+		array[move[1], move[2]] = 0
+
+		if abs(array[move[3], move[4]]) == 1 and move[3] in [0, 7]:
+			array[move[3], move[4]] *= 5
+
+		if move[0] == 1:
+			array[move[1], move[4]] = 0
+
+	else:
+		array[move[1], move[3]] = array[move[1], move[2]]
+		array[move[1], move[5]] = array[move[1], move[4]]
+		array[move[1], move[2]] = 0
+		array[move[1], move[4]] = 0
+
+@jit(nopython=True)
+def get_moves(array, turn, history):
+	history1 = np.vstack((history, array.reshape((1, 8, 8))))
+	moves = get_moves_(array, turn, history)
+	filt_moves = np.zeros((0, 6), dtype=np.int8)
+
+	for i, move in enumerate(moves):
+		array1 = array.copy()
+		play(array1, move)
+
+		moves1 = get_moves_(array1, not turn, history1)
+		delete = False
+
+		for move1 in moves1:
+			if move1[0] == 0:
+				if abs(array1[move1[3], move1[4]]) == 6 and is_turn(array1[move1[3], move1[4]], turn):
+					delete = True
+					break
+
+				if move[0] == 2:
+					if move1[3] == move[1] and move1[4] in (move[2], move[5]):
+						delete = True
+						break
+
+		if not delete:
+			move = move.copy()
+			filt_moves = np.vstack((filt_moves, move.reshape((1, 6))))
+
+	return filt_moves
